436f6b3a3cd358c56d20b090dee8e923937f0e01
Milan Matull <milan@factorial.io>
2020-02-05 17:13:43 +0100

    fix: add missing section header

---
+++ src/components/UiSectionHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="my-4 bg-gray-200">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+
+export default Vue.extend({});
+</script>
